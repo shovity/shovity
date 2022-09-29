a64764cfeeb2f9757e91b4b4684a6841e91cbe3c
shovity <shovity@gmail.com>
2022-09-29 10:53:30 +0700

    update time line

--- js/main.js
+++ js/main.js
@@ -141,7 +141,7 @@
             var timeinterval = setInterval(updateClock, 1000);
         }
         // set your wedding date here
-        var deadline = '2022-10-10T04:00:00.000Z';
+        var deadline = '2022-10-11T04:00:00.000Z';
         if (countdown){
             initializeClock('timer', deadline);
         }
